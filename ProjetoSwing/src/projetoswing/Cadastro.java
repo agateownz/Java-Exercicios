b8d71a049bff8f49d8df78e89fd3a235630e3e5a
Luís Gustavo <luis.gustavo.dias@hotmail.com>
2017-09-02 10:27:39 -0300

    Adicionado novo projeto utilizando a biblioteca Swing para criação de GUI.

---
+++ ProjetoSwing/src/projetoswing/Cadastro.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoswing;
+
+/**
+ *
+ * @author Luís
+ */
+public class Cadastro {
+    private String name;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String email;
+    private String sex;
+    
+    private String reason;
+    
+    private boolean german;
+    private boolean spanish;
+    private boolean english;
+    private boolean french;
+    private boolean italian;
+    private boolean japanese;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setGerman(boolean german) {
+        this.german = german;
+    }
+
+    public void setSpanish(boolean spanish) {
+        this.spanish = spanish;
+    }
+    
+        public void setEnglish(boolean english) {
+        this.english = english;
+    }
+
+    public void setFrench(boolean french) {
+        this.french = french;
+    }
+
+    public void setItalian(boolean italian) {
+        this.italian = italian;
+    }
+
+    public void setJapanese(boolean japanese) {
+        this.japanese = japanese;
+    }
+    
+    
+    
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Repositório para armazenar alguns exercícios realizados durante o aprendizado 
 - Fatorial: Exercício clássico para calcular o fatorial de um número.
 - Fibonacci: Exercício clássico para calcular a sequência de fibonacci.
 - ControleFinanceiro: Exercício proposto para gerenciar uma Conta Bancária.
+- ProjetoSwing: Interface gráfica simples utilizando a biblioteca Swing.
